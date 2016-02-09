0288b5fdfc204c38298f92b72c6c85deb039d852
Bobby Craig <robtcraig@gmail.com>
2016-02-09 00:08:08 -0500

    updates for piper viewing

--- ics-parser/index.php
+++ ics-parser/index.php
@@ -1,18 +1,5 @@
 <?php
-/**
- * This example demonstrates how the Ics-Parser should be used.
- *
- * PHP Version 5
- *
- * @category Example
- * @package  Ics-parser
- * @author   Martin Thoma <info@martin-thoma.de>
- * @license  http://www.opensource.org/licenses/mit-license.php  MIT License
- * @version  SVN: <svn_id>
- * @link     http://code.google.com/p/ics-parser/
- * @example  $ical = new ical('MyCal.ics');
- *           print_r( $ical->get_event_array() );
- */
+
 require 'class.iCalReader.php';
 
 $ical   = new ICal('MyCal.ics');
